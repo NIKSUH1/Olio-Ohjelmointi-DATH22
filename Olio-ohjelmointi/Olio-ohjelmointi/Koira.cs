e251220a90ee031f25c71664eef7037a933acdcd
Niko Holmi <turhaa111@outlook.com>
2023-01-09 12:21:59 +0200

    Olio harjoittelua: -olion luonti --Construcktori

---
+++ Olio-ohjelmointi/Olio-ohjelmointi/Koira.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_ohjelmointi
+{
+    internal class Koira
+    {
+        public string Nimi;
+        public string Rotu;
+        public int Ikä;
+
+        // Constructori jolla pakotetaan olion luonti tilanteessa
+        // Antamaan tarvittavat tiedot oliolle ( nimi ro,u ja ikä)
+        public Koira(string nimi, string rotu, int ikä)
+        {
+            Nimi = nimi;
+            Rotu = rotu;
+            Ikä = ikä;
+        }
+
+        public void HaeTiedot()
+        {
+            Console.WriteLine("Koira nimi on " + Nimi);
+            Console.WriteLine("Koira ikä on " + Ikä);
+            Console.WriteLine("Koira rotu on " + Rotu);
+        }
+        public void Hauku()
+        {
+            Console.WriteLine(Nimi + " Woof woof");
+        }
+    }
+}
--- Olio-ohjelmointi/Olio-ohjelmointi/Program.cs
+++ Olio-ohjelmointi/Olio-ohjelmointi/Program.cs
@@ -10,8 +10,20 @@ namespace Olio_ohjelmointi
     {
         static void Main(string[] args)
         {
+            Koira koira1 = new Koira("Clara", "American bulldog", 1); // luodaan uusi koira olio/instanssi
 
+            koira1.HaeTiedot();
 
+            Console.WriteLine();
+
+            Koira Koira2 = new Koira("Jerry", "Mittelspitz", 13);
+
+            Koira2.HaeTiedot();
+
+            Console.WriteLine();
+
+            koira1.Hauku();
+            Koira2.Hauku();
         }
     }
 }
