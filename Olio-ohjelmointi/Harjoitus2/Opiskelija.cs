7911ea48f109a06b634413086e7b3b01c8ed77b2
Niko Holmi <turhaa111@outlook.com>
2023-01-09 13:39:51 +0200

    Harjoitus2 -olio harjoittelua

---
+++ Olio-ohjelmointi/Harjoitus2/Opiskelija.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    internal class Opiskelija
+    {
+        public string Nimi;
+        public string OpiskelijaID;
+        public int OpintoPisteet;
+
+        public Opiskelija(string nimi, string opiskelijaId, int opintoPisteet)
+        {
+            Nimi = nimi;
+            OpiskelijaID = opiskelijaId;
+            OpintoPisteet = opintoPisteet;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Opiskelijan nimi on " + Nimi);
+            Console.WriteLine("Opiskelijan ID on " + OpiskelijaID);
+            Console.WriteLine("Opiskelijan tarviima OP määrä " + OpintoPisteet);
+        }
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Jeesus", "666", 240);
+
+            opiskelija1.TulostaData();
+        }
+    }
+}
