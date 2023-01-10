59215f8b2e6eee983a4cef5e23dec34da2646b91
Niko Holmi <turhaa111@outlook.com>
2023-01-10 11:40:54 +0200

    Harjoitus5 -laskin

---
+++ Olio-ohjelmointi/Harjoitus5/Laskin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus5
+{
+    internal class Laskin
+    {
+        public static float summa(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float erotus(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+        public static float jakolasku(float a, float b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Laskin.summa(14, 15));
+            Console.WriteLine(Laskin.erotus(16, 15));
+            Console.WriteLine(Laskin.kertolasku(14, 2));
+            Console.WriteLine(Laskin.jakolasku(14, 7));
+
+        }
+    }
+}
