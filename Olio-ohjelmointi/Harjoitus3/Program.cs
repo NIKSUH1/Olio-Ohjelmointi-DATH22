81f6ef04f7435f18d1bcee1c394a6fe9540d1c20
Niko Holmi <turhaa111@outlook.com>
2023-01-13 12:25:39 +0200

    Harjoitus3 -kiuas

---
+++ Olio-ohjelmointi/Harjoitus3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kiuas kiuas = new kiuas();
+
+            while (true)
+            {
+                string syöte = Console.ReadLine();
+                if (syöte == "päälle")
+                {
+                    kiuas.PistäPäälle(true);
+                }
+                if (syöte == "pois päältä")
+                {
+                    kiuas.PistäPäälle(false);
+                }
+
+                if (syöte == "lisää lämpöä")
+                {
+                    kiuas.LisääLämpöä();
+                }
+
+                if (syöte == "heitä löylyä")
+                {
+                    kiuas.IlmanKosteus();
+                }
+            }
+        }
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus3/kiuas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus3
+{
+    internal class kiuas
+    {
+        public bool onkoPäällä;
+        public int lämpö;
+        public float kosteus;
+
+        public void PistäPäälle(bool on)
+        {
+            if (on)
+            {
+                onkoPäällä = true;
+                Console.WriteLine("Laitoit kiukaan päälle");
+                lämpö = 1;
+            }
+            else if (!on)
+            {
+                onkoPäällä = false;
+                Console.WriteLine("Laitoit kiukaan pois päältä");
+            }
+        }
+
+        public void LisääLämpöä()
+        {
+            lämpö = lämpö + 10;
+            Console.WriteLine("lämpötila on " + lämpö);
+        }
+        public void IlmanKosteus()
+        {
+            kosteus = kosteus + 0.1f;
+            Console.WriteLine("Saunan kosteus on " + kosteus);
+        }
+    }
+}
