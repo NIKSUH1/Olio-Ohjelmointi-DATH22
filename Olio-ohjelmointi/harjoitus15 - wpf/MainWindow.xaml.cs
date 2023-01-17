76da52990cbc247f7c34856814c0d44b71dc5a4a
Niko Holmi <turhaa111@outlook.com>
2023-01-17 10:40:02 +0200

    harjoitus15 -WPF harjotusta -auto tehtävä

---
+++ Olio-ohjelmointi/harjoitus15 - wpf/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace harjoitus15___wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int kuormaAutot = 0;
+        private int henkilöAutot = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btn_KuormaAutot_Click(object sender, RoutedEventArgs e)
+        {
+            kuormaAutot = kuormaAutot + 1;
+            txt_KuormaAutotMäärä.Text = Convert.ToString(kuormaAutot);
+        }
+
+        private void btn_HenkilöAutot_Click(object sender, RoutedEventArgs e)
+        {
+            henkilöAutot = henkilöAutot +1;
+            txt_HenkilöAutotMäärä.Text = Convert.ToString(henkilöAutot);
+            
+        }
+    }
+}
---
+++ Olio-ohjelmointi/wpfapp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfapp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string käyttäjänNimi;
+
+        // vähän niin kuin unityn start metodi
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_button_Click(object sender, RoutedEventArgs e)
+        {
+            käyttäjänNimi = input_Nimi.Text;
+            txt_EkaTeksti.Text = "Nimesi on = " + käyttäjänNimi;
+        }
+    }
+}
