d5edcfacafce157b3d155e6651acea524ee19006
Niko Holmi <turhaa111@outlook.com>
2023-01-24 11:43:19 +0200

    Harjoitus17 -Talo WPF

---
+++ Olio-ohjelmointi/Harjoitus17/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_KeittiönValo_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_KeittiöV.Text == "Keittiön valo ei ole päällä")
+            {
+                txt_KeittiöV.Text = "Keittiön valo on päällä";
+            }
+            else
+            {
+                txt_KeittiöV.Text = "Keittiön valo ei ole päällä";
+            }
+        }
+
+        private void btn_OlohuoneenValo_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_OlohuoneenV.Text == "Olohuoneen valo ei ole päällä")
+            {
+                txt_OlohuoneenV.Text = "Olohuoneen valo on päällä";
+            }
+            else
+            {
+                txt_OlohuoneenV.Text = "Olohuoneen valo ei ole päällä";
+            }
+        }
+
+        private void btn_Ovi_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt_Ovi.Text == "Ovi on auki")
+            {
+                txt_Ovi.Text = "Ovi on kiinni";
+            }
+            else
+            {
+                txt_Ovi.Text = "Ovi on auki";
+            }
+        }
+
+        private void sld_Lämpötila_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txt_lämpötilaTeksti.Text = "Lämpötila: " + sld_Lämpötila.Value;
+        }
+    }
+}
